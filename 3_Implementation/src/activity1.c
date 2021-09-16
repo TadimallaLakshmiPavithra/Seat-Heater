4a1f4b1d94ac453622024f19c78f2b40f67350fb
Git_username <Git_user_email>
2021-09-16 16:24:35 +0530

    update

---
+++ 3_Implementation/src/activity1.c
@@ -0,0 +1,24 @@
+
+int main(void){
+
+    DDRD|=(1<<PD6);//set D6=1 for LED
+    DDRD&=~(1<<PD4);//clear bit
+    PORTD|=(1<<PD4);//set bit
+    DDRD&=~(1<<PD5);//clear bit
+    PORTD|=(1<<PD5);//set bit
+    while(1)
+    {
+        if(!((PIND&(1<<PD4)) || (PIND&(1<<PD5)))) //switch press
+        {
+            PORTD|=(1<<PD6);
+            _delay_ms(2000);
+        }
+        else{
+            PORTD&=~(1<<PD6);
+            _delay_ms(2000);
+        }
+    }
+
+
+   return 0;
+}
