76ab1832e950974cb563b328b9883e408850fe76
Git_username <Git_user_email>
2021-09-16 17:56:40 +0530

    update

---
+++ 3_Implementation/inc/activity2.h
@@ -0,0 +1,13 @@
+#ifndef ACTIVITY2_H_INCLUDED
+#define ACTIVITY2_H_INCLUDED
+
+void ADC_Init();
+/**
+ * @brief to read the ADC values
+ * 
+ * @param ch 
+ * @return uint16_t 
+ */
+uint16_t ADC_Read(uint8_t ch);
+
+#endif // ACTIVITY2_H_INCLUDED
